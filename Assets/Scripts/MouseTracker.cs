6093f53d4b2f3476bce54b9766b528608d787f6a
GSheldon262 <gsheldon@ltu.edu>
2025-04-03 18:56:24 -0400

    Resolution Fix

--- Assets/Scripts/MouseTracker.cs
+++ Assets/Scripts/MouseTracker.cs
@@ -39,7 +39,7 @@ public class MouseTracker : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Screen.SetResolution(1920, 1080, true);
     }
 
     // Update is called once per frame
